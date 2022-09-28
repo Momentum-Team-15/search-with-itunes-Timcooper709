8c938c0a2b77678a9f136379e06b0305e638c7b3
Timcooper709 <112970939+Timcooper709@users.noreply.github.com>
2022-09-28 10:37:50 -0400

    search fuction

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("hello world");
+console.log("hello world"); 
+const url = 'https://proxy-itunes-api.glitch.me/search?term=jack+johnson'
+
+const dataContainer = document.querySelector("#data_container")
+dataContainer.id = "data"
+
+
+
+
+
+fetch(url, {
+   method: 'GET',
+   headers: { 'Content-Type': 'application/json' },
+})
+
+   .then(function (response) {
+      return response.json()
+   })
+
+   .then(function (data) {
+      for (let music of data.results) {
+         console.log(music.trackName)
+         let musicDiv = document.createElement("div");
+         dataContainer.appendChild(musicDiv);
+         musicDiv.id = "data"
+
+         let title = document.createElement("p");
+         title.innerText = `${music.trackName}`;
+         musicDiv.appendChild(title);
+
+      /*  let picture = document.createElement("img");
+         album.innerText = `$(music.picture)`;
+         musicDiv.appendChild(picture)
+         */
+
+
+      }
+   })
+
+
+
+
+
+
+
+
+
+
