2e780856542dd164ca9739a824a346513f87e32c
Timcooper709 <112970939+Timcooper709@users.noreply.github.com>
2022-09-29 10:57:50 -0400

    title

--- main.js
+++ main.js
@@ -1,56 +1,66 @@
-console.log("hello world"); 
+console.log("hello world");
 const search = document.createElement('div');
 
-let url = 'https://proxy-itunes-api.glitch.me/search?term='
+let input = document.createElement('userInput');
+input.type = 'text';
+search.appendChild(input);
+
+let searchBaseUrl = 'http://itunes.apple.com/search?term=';
+
+
 
 const dataContainer = document.querySelector("#data_container")
 dataContainer.id = "data"
 
 //adding eventlistener for search
-let userInput = document.querySelector(".search");
-let searchForm = document.querySelector("#search-box");
+//let userInput = document.querySelector(".searchBox");
+//let searchForm = document.querySelector("#search-box");
 
+let searchForm = document.querySelector('#search-form')
 searchForm.addEventListener("submit", (event) => {
-event.preventDefault();
-let artistNow = userInput.value;
-appleMusicSearch(url);
+   event.preventDefault();
+   console.log('submitted',)
+   let searchBox = document.querySelector('#search-box')
+   let searchUrl = `${searchBaseUrl}${searchBox.value}`
+   console.log(searchUrl)
+
+
 
 });
 
 
+function getSearchResults(url) {
 
-
-fetch(url, {
-   method: 'GET',
-   headers: { 'Content-Type': 'application/json' },
-})
-
-   .then(function (response) {
-      return response.json()
+   fetch(url, {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
    })
+
+      .then(function (response) {
+         return response.json()
+      })
+   
 
    .then(function (data) {
-      for (let music of data.results) {
-         console.log(music.trackName)
-         let musicDiv = document.createElement("div");
-         dataContainer.appendChild(musicDiv);
-         musicDiv.id = "data"
+         for (let music of data.results) {
+            console.log(music.trackName)
+            let musicDiv = document.createElement("div");
+            dataContainer.appendChild(musicDiv);
+            musicDiv.id = "data"
 
-         let title = document.createElement("p");
-         title.innerText = `${music.trackName}`;
-         musicDiv.appendChild(title);
+            let title = document.createElement("p");
+            title.innerText = `${music.trackName}`;
+            musicDiv.appendChild(title);
 
-      /*  let picture = document.createElement("img");
-         album.innerText = `$(music.picture)`;
-         musicDiv.appendChild(picture)
-         */
-
-
-      }
-   })
+            /*  let picture = document.createElement("img");
+               album.innerText = `$(music.picture)`;
+               musicDiv.appendChild(picture)
+               */
 
 
-
+         }
+      })
+      
 
 
 
@@ -58,3 +68,6 @@ fetch(url, {
 
 
 
+
+
+   }
